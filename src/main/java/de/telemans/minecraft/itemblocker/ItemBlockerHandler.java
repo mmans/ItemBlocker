48c328ed1760bf62b331c7732df2e483f1486a63
Marco Mans <marco.mans@engie.com>
2019-11-10 00:05:25 +0100

    WIP
    - Load config complete
    - onPickupItem complete
    - onInventory wip

--- src/main/java/de/telemans/minecraft/itemblocker/ItemBlockerHandler.java
+++ src/main/java/de/telemans/minecraft/itemblocker/ItemBlockerHandler.java
@@ -1,33 +1,93 @@
 package de.telemans.minecraft.itemblocker;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.*;
+import org.bukkit.event.player.PlayerInventoryEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ItemBlockerHandler implements Listener {
-    private JavaPlugin plugin;
+import java.util.HashMap;
+import java.util.Map;
 
-    ItemBlockerHandler(JavaPlugin plugin) {
+public class ItemBlockerHandler implements Listener {
+    private ItemBlockerPlugin plugin;
+    private ItemBlockerConfig config;
+
+    ItemBlockerHandler(ItemBlockerPlugin plugin) {
         this.plugin = plugin;
+        this.config = plugin.getItemBlockerConfig();
     }
 
     @EventHandler
     public void onItemPickup(PlayerPickupItemEvent evt) {
-        int itemid = evt.getItem().getEntityId();
+
+        if (config.isDisabledForOps()) {
+            return;
+        }
+
         Material item = evt.getItem().getItemStack().getType();
-        String playername = evt.getPlayer().getDisplayName();
 
-        if (item == Material.SAND) {
+        if (config.isBlocked(item)) {
+            ItemBlockerItemConfig itemConfig = config.getBlockedItemConfig(item);
 
-            plugin.getLogger().info("Player " + playername + " Picked up a " + item + " (" + itemid + ")");
-
-            if (!evt.getPlayer().isOp()) {
-                evt.getPlayer().sendMessage("§c Sorry " + item + " items are not allowed!");
-                evt.getItem().remove();
-                evt.setCancelled(true);
+            if (itemConfig.getMaxAmount() > 0) {
+                // Check if player has less than allowed amount
+                int currentItemAmount = getCurrentInventryAmount(evt.getPlayer(), item);
+                if (currentItemAmount < itemConfig.getMaxAmount()) {
+                    int maxAddition = itemConfig.getMaxAmount() - currentItemAmount;
+                    if (maxAddition < evt.getItem().getItemStack().getAmount()){
+                        evt.getPlayer().sendMessage(ChatColor.ITALIC.toString() + "Only adding " + maxAddition + " more " + item + "-block(s)");
+                    }
+                    ItemStack addItemStack = new ItemStack(item, maxAddition);
+                    evt.getPlayer().getInventory().addItem(addItemStack);
+                    evt.setCancelled(true);
+                    evt.getItem().remove();
+                    return;
+                }
             }
-        }    }
+            evt.getPlayer().sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD.toString() + itemConfig.getBlockText());
+            evt.getItem().remove();
+            evt.setCancelled(true);
+        }
+    }
+
+    private int getCurrentInventryAmount(Player player, Material mat) {
+        int currentAmount = 0;
+        for (ItemStack is : player.getInventory()) {
+            if (is != null && is.getType() == mat) {
+                currentAmount += is.getAmount();
+            }
+        }
+        return currentAmount;
+    }
+
+    @EventHandler
+    public void onInventoryChange(InventoryCreativeEvent evt) {
+        Map<Material, Integer> uniqueInventory = new HashMap<>();
+
+        plugin.getLogger().info(("Cursor = " + evt.getCursor()));
+        // TODO: Only react on cursor!!!
+
+        for (ItemStack is : evt.getInventory()){
+            if (is != null){
+                if (!uniqueInventory.containsKey(is.getType())){
+                    uniqueInventory.put(is.getType(), is.getAmount());
+                } else {
+                    uniqueInventory.put(is.getType(), uniqueInventory.get(is.getType())+ is.getAmount());
+                }
+            }
+        }
+
+        for (Map.Entry<Material, Integer> a : uniqueInventory.entrySet()){
+            plugin.getLogger().info(a.getKey() + " --> " + a.getValue());
+        }
+
+    }
 }
 
--- src/main/java/de/telemans/minecraft/itemblocker/ItemBlockerPlugin.java
+++ src/main/java/de/telemans/minecraft/itemblocker/ItemBlockerPlugin.java
@@ -1,66 +1,33 @@
 package de.telemans.minecraft.itemblocker;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.MemorySection;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ItemBlockerPlugin extends JavaPlugin {
-    private final ItemBlockerHandler handler = new ItemBlockerHandler(this);
-    private final Map<Material, ItemBlockerConfig> blockedItems = new HashMap<>();
+    private ItemBlockerHandler handler;
+    private ItemBlockerConfig config;
 
     @Override
     public void onEnable() {
         super.onEnable();
-        getLogger().info("▄▄▄▄▄▄▄▄ .▄▄▌  ▄▄▄ .• ▌ ▄ ·.  ▄▄▄·  ▐ ▄ .▄▄ ·");
-        getLogger().info("•██  ▀▄.▀·██•  ▀▄.▀··██ ▐███▪▐█ ▀█ •█▌▐█▐█ ▀.");
-        getLogger().info("▐█.▪▐▀▀▪▄██▪  ▐▀▀▪▄▐█ ▌▐▌▐█·▄█▀▀█ ▐█▐▐▌▄▀▀▀█▄");
-        getLogger().info("▐█▌·▐█▄▄▌▐█▌▐▌▐█▄▄▌██ ██▌▐█▌▐█ ▪▐▌██▐█▌▐█▄▪▐█");
-        getLogger().info("▀▀▀  ▀▀▀ .▀▀▀  ▀▀▀ ▀▀  █▪▀▀▀ ▀  ▀ ▀▀ █▪ ▀▀▀▀");
-        getLogger().info("Enabled ItemBlockerPlugin! Welcome my friend!");
-
         // Load config
         saveDefaultConfig();
-        loadConfig();
+        config = new ItemBlockerConfig(getConfig(), this);
 
         // Register Commands
         getCommand("ibshow").setExecutor(new ItemBlockerCommandExecuter());
 
         // Register Handler
+        handler =  new ItemBlockerHandler(this);
         getServer().getPluginManager().registerEvents(handler, this);
     }
 
-    private void loadConfig() {
-        Object items = getConfig().get("items");
-
-        if (items instanceof MemorySection){
-            MemorySection itemlist = (MemorySection) items;
-            for (String item : itemlist.getKeys(false)){
-                Material mat = Material.matchMaterial(item);
-                if (mat != null){
-
-                } else {
-                    getLogger().severe("Material " + item + " not found!");
-                }
-            }
-        } else{
-            getLogger().severe("items section not well formatted!");
-        }
-
-        getLogger().info(items.getClass().getCanonicalName());
-
-//        for (Map<String, Object> item : items) {
-//            if (item.containsKey("blockid"))
-//
-//
-//            for (String a : item.keySet()) {
-//
-//                getLogger().info(a  + " -> " + item.get(a));
-//            }
-//        }
+    public ItemBlockerConfig getItemBlockerConfig(){
+        return config;
     }
 }
