2b7273af4531fffef10bc80940418ed0e51b15f7
Marco Mans <marco.mans@engie.com>
2019-11-10 14:16:54 +0100

    v1.0

--- src/main/java/de/telemans/minecraft/itemblocker/ItemBlockerHandler.java
+++ src/main/java/de/telemans/minecraft/itemblocker/ItemBlockerHandler.java
@@ -1,8 +1,11 @@
 package de.telemans.minecraft.itemblocker;
 
+import com.sun.tools.javac.jvm.Items;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.*;
@@ -14,6 +17,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ItemBlockerHandler implements Listener {
     private ItemBlockerPlugin plugin;
@@ -32,32 +36,37 @@ public class ItemBlockerHandler implements Listener {
         }
 
         Material item = evt.getItem().getItemStack().getType();
+        boolean shouldCancel = handleEvent(evt, evt.getPlayer(), evt.getItem().getItemStack());
 
-        if (config.isBlocked(item)) {
-            ItemBlockerItemConfig itemConfig = config.getBlockedItemConfig(item);
-
-            if (itemConfig.getMaxAmount() > 0) {
-                // Check if player has less than allowed amount
-                int currentItemAmount = getCurrentInventryAmount(evt.getPlayer(), item);
-                if (currentItemAmount < itemConfig.getMaxAmount()) {
-                    int maxAddition = itemConfig.getMaxAmount() - currentItemAmount;
-                    if (maxAddition < evt.getItem().getItemStack().getAmount()){
-                        evt.getPlayer().sendMessage(ChatColor.ITALIC.toString() + "Only adding " + maxAddition + " more " + item + "-block(s)");
-                    }
-                    ItemStack addItemStack = new ItemStack(item, maxAddition);
-                    evt.getPlayer().getInventory().addItem(addItemStack);
-                    evt.setCancelled(true);
-                    evt.getItem().remove();
-                    return;
-                }
-            }
-            evt.getPlayer().sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD.toString() + itemConfig.getBlockText());
+        if (shouldCancel){
             evt.getItem().remove();
             evt.setCancelled(true);
         }
+//        if (config.isBlocked(item)) {
+//            ItemBlockerItemConfig itemConfig = config.getBlockedItemConfig(item);
+//
+//            if (itemConfig.getMaxAmount() > 0) {
+//                // Check if player has less than allowed amount
+//                int currentItemAmount = getCurrentInventryAmount(evt.getPlayer(), item);
+//                if (currentItemAmount < itemConfig.getMaxAmount()) {
+//                    int maxAddition = itemConfig.getMaxAmount() - currentItemAmount;
+//                    if (maxAddition < evt.getItem().getItemStack().getAmount()) {
+//                        evt.getPlayer().sendMessage(ChatColor.ITALIC.toString() + "Only adding " + maxAddition + " more " + item + "-block(s)");
+//                    }
+//                    ItemStack addItemStack = new ItemStack(item, maxAddition);
+//                    evt.getPlayer().getInventory().addItem(addItemStack);
+//                    evt.setCancelled(true);
+//                    evt.getItem().remove();
+//                    return;
+//                }
+//            }
+//            evt.getPlayer().sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD.toString() + itemConfig.getBlockText());
+//            evt.getItem().remove();
+//            evt.setCancelled(true);
+//        }
     }
 
-    private int getCurrentInventryAmount(Player player, Material mat) {
+    private int getCurrentInventryAmount(HumanEntity player, Material mat) {
         int currentAmount = 0;
         for (ItemStack is : player.getInventory()) {
             if (is != null && is.getType() == mat) {
@@ -68,26 +77,38 @@ public class ItemBlockerHandler implements Listener {
     }
 
     @EventHandler
-    public void onInventoryChange(InventoryCreativeEvent evt) {
-        Map<Material, Integer> uniqueInventory = new HashMap<>();
+    public void onInventoryDrag(InventoryClickEvent evt) {
+        Material placedItem = evt.getCursor().getType();
+        boolean shouldCancel = handleEvent(evt, evt.getWhoClicked(), evt.getCursor());
 
-        plugin.getLogger().info(("Cursor = " + evt.getCursor()));
-        // TODO: Only react on cursor!!!
+        if (shouldCancel){
+            evt.setCancelled(true);
+        }
+    }
 
-        for (ItemStack is : evt.getInventory()){
-            if (is != null){
-                if (!uniqueInventory.containsKey(is.getType())){
-                    uniqueInventory.put(is.getType(), is.getAmount());
-                } else {
-                    uniqueInventory.put(is.getType(), uniqueInventory.get(is.getType())+ is.getAmount());
+    private boolean handleEvent(Event evt, HumanEntity player, ItemStack item){
+        if (config.isBlocked(item.getType())) {
+            ItemBlockerItemConfig itemConfig = config.getBlockedItemConfig(item.getType());
+
+            if (itemConfig.getMaxAmount() > 0) {
+                // Check if player has less than allowed amount
+                int currentItemAmount = getCurrentInventryAmount(player, item.getType());
+                if (currentItemAmount < itemConfig.getMaxAmount()) {
+                    int maxAddition = itemConfig.getMaxAmount() - currentItemAmount;
+                    int willAddAmount = item.getAmount();
+                    if (maxAddition < item.getAmount()) {
+                        player.sendMessage(ChatColor.ITALIC.toString() + "Only adding " + maxAddition + " more " + item + "-block(s)");
+                        willAddAmount = maxAddition;
+                    }
+                    ItemStack addItemStack = new ItemStack(item.getType(), willAddAmount);
+                    player.getInventory().addItem(addItemStack);
+                    return true; // Notify caller that event needs to be cancelled
                 }
             }
+            player.sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD.toString() + itemConfig.getBlockText());
+            return true;
         }
-
-        for (Map.Entry<Material, Integer> a : uniqueInventory.entrySet()){
-            plugin.getLogger().info(a.getKey() + " --> " + a.getValue());
-        }
-
+        return false;
     }
 }
 
